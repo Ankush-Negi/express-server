96de6c2929a3f8a2401d5a8605d4a5e0cae63c36
Ankush-Negi <ankush.negi@successive.tech>
2020-02-07 13:12:30 +0530

    fix:feature branch updated

--- dist/patterns/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var diamond_js_1 = require("./diamond.js");
-exports.diamond = diamond_js_1.default;
-var equilateral_js_1 = require("./equilateral.js");
-exports.equilateral = equilateral_js_1.default;
-//# sourceMappingURL=index.js.map
--- dist/utils/permissions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function hasPermission(moduleName, role, permissionType) {
-    const roles = moduleName[permissionType];
-    return roles.includes(role);
-}
-exports.default = hasPermission;
-//# sourceMappingURL=permissions.js.map
