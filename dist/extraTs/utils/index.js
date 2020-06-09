18c8d3c8af491f055da87b8a2c6eca099b164bb0
Ankush-Negi <ankush.negi@successive.tech>
2020-06-09 17:10:22 +0530

    fix: node_module and dist remove

--- dist/extraTs/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var permissions_js_1 = require("./permissions.js");
-exports.hasPermission = permissions_js_1.default;
-var validation_js_1 = require("./validation.js");
-exports.validateUsers = validation_js_1.default;
-//# sourceMappingURL=index.js.map
--- dist/src/libs/routes/notFoundRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function notFoundRoute(req, res, next) {
-    const err = new Error('Not Found');
-    next(err);
-}
-exports.default = notFoundRoute;
-//# sourceMappingURL=notFoundRoute.js.map
